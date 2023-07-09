c672ca988b1fddae5c2668cb2c686980ca588775
Manohar <manohar9600@gmail.com>
2023-07-09 20:50:06 +0530

    added more files download

--- main.py
+++ main.py
@@ -23,37 +23,52 @@ def get_filing_urls(cik):
     return company_filings_urls[cik]
 
 
-def download_company_files(cik):
+def download_file(link, folder_path):
     headers = {
        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
     }
-    os.makedirs(f'data/{cik}', exist_ok=True)
+    r = requests.get(link, headers=headers)
+    file_name = link.split('/')[-1]
+    file_path = os.path.join(folder_path, file_name)
+    with open(file_path, 'wb') as f:
+        f.write(r.content)
+    return r.text
+
+
+def download_company_files(cik):
     filing_urls = get_filing_urls(cik)
     for filing_url in filing_urls:
-        file_name = filing_url.split('/')[-1].replace('.txt', '')
-        file_path = f'data/{cik}/{file_name}.json'
-        if os.path.exists(file_path):
+        html_master = "/".join(filing_url.split('/')[:-1])
+        index_url = filing_url.replace(".txt", "-index-headers.html")
+        folder_path = f"data/{cik}/" + html_master.split('/')[-1]
+        table_json_path = os.path.join(folder_path, "tables.json")
+        if os.path.exists(table_json_path):
             continue
+        
         logger.info(f"processing {filing_url}")
-        r = requests.get(filing_url, headers=headers)
-        if r.status_code == 200:
-            tables = get_html_tables(r.text)
-            tables = classify_tables(tables)
-            tables = construct_proper_tables(tables)
-            file_name = filing_url.split('/')[-1].replace('.txt', '')
-            file_path = f'data/{cik}/{file_name}.json'
-            data = {
-                'url': filing_url,
-                'tables': tables
-            }
-            with open(file_path, 'w') as f:
-                json.dump(data, f, indent=4)
-            with open(file_path.replace(".json", ".html"), 'w') as f:
-                f.write(r.text)
+        os.makedirs(folder_path, exist_ok=True)
+        index_content = download_file(index_url, folder_path)
+        soup = BeautifulSoup(index_content, "html.parser")
+        links = soup.find_all("a")
+        for link in links:
+            if "Financial_Report.xlsx" in link['href']:
+                download_file(html_master + "/" + "Financial_Report.xlsx", folder_path)
+
+        report_html = download_file(html_master + "/" + links[0]['href'], folder_path)
+        tables = get_html_tables(report_html)
+        tables = classify_tables(tables)
+        tables = construct_proper_tables(tables)
+        data = {
+            'url': filing_url,
+            'tables': tables
+        }
+        with open(table_json_path, 'w') as f:
+            json.dump(data, f, indent=4)
 
 
 if __name__ == "__main__":
-    ciks = ['mmm', 'aos', 'abt', 'abbv', 'acn', 'atvi', 'adm']
+    ciks = ['aapl']
+    # ciks = ['mmm', 'aos', 'abt', 'abbv', 'acn', 'atvi', 'adm']
     for cik in ciks:
         logger.info(f"processing company, cik:{cik}")
         download_company_files(cik)
