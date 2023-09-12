6a263b2154b2d4753738e61a09bdb4d7c33a9d64
manohar <manohar9600@outlook.com>
2023-09-12 21:58:00 +0530

    missed code

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import os
 from collections import Counter
+import openpyxl
+from openpyxl.styles import Font, Alignment
+
 
 def most_occuring_element(lst):
     if len(lst) == 0:
@@ -28,3 +31,32 @@ def get_folder_names(path):
 
     folders.reverse()  # Reversing the list to get the correct order
     return folders
+
+
+def write_to_excel(tables, target_path, var_columns = ['A', 'B']):
+    workbook = openpyxl.Workbook()
+    del workbook['Sheet']
+    for i, table in enumerate(tables):
+        worksheet = workbook.create_sheet(str(i) + "_" + table['class'])
+
+        # Set the width
+        # for val_col in var_columns:
+        worksheet.column_dimensions['A'].width = 50
+        worksheet.column_dimensions['B'].width = 30
+
+        # Create a bold font style for column headers
+        bold_font = Font(bold=True)
+
+        # Loop through the list of lists and write data to the worksheet
+        for row_index, row_data in enumerate([table['header']] + table['body'], start=1):
+            for col_index, cell_value in enumerate(row_data, start=1):
+                cell = worksheet.cell(row=row_index, column=col_index, value=cell_value)
+                # Apply bold font to the header row (first row)
+                if row_index == 1:
+                    cell.font = bold_font
+                    cell.alignment = Alignment(wrap_text=True,vertical='top')
+                if col_index == 1:
+                    # cell.style.alignment.wrap_text=True
+                    cell.alignment = Alignment(wrap_text=True,vertical='top')
+
+    workbook.save(target_path)
