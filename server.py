9457333574e6d543f3f02f4d64759897074fee43
manohar <manohar9600@outlook.com>
2023-09-28 17:43:17 +0530

    updated server code with new support

--- server.py
+++ server.py
@@ -3,10 +3,12 @@ import tornado.web
 import os
 import json
 from glob import glob
+import pandas as pd
+import psycopg2
 from loguru import logger
 
 
-master_folder = 'data'
+master_folder = 'data/current'
 class AllHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
@@ -29,6 +31,65 @@ class AllHandler(tornado.web.RequestHandler):
 
         self.set_header('Content-Type', 'application/json')
         self.write(data)
+
+
+def get_company_data(company_symbol):
+    conn = psycopg2.connect(database="ProdDB",
+                                host="localhost",
+                                user="postgres",
+                                password="manu1234",
+                                port="5432")
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT id FROM companies where symbol='{company_symbol}'")
+    comp_id = cursor.fetchone()[0]
+    
+    query = f"SELECT * FROM values INNER JOIN variables ON values.variableid = variables.id WHERE values.companyid = {comp_id}"
+    cursor.execute(query)
+    df = pd.DataFrame(cursor, columns=[desc[0] for desc in cursor.description])
+    conn.close()
+    return df
+
+
+class CompanyData(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header("Access-Control-Allow-Headers", "Content-Type")
+        self.set_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+
+    def options(self):
+        # This method handles the pre-flight OPTIONS request.
+        # It simply responds with the CORS headers.
+        self.set_status(204)
+        self.finish()
+    
+    def post(self):
+        logger.info("--- company data request received ---")
+        data = json.loads(self.request.body)
+        logger.info(f"got request for data of company:{data['company']}")
+        company_data = get_company_data(data['company'])
+        columns = company_data
+        df = get_company_data(data['company'])
+        tables = []
+        for table_name, table_grp in df.groupby('table'):
+            columns = list(df.period.unique())
+            table = []
+            for _, grp in table_grp.groupby('variableid'):
+                table_row = ["" for _ in range(len(columns)+1)]
+                table_row[0] = grp.iloc[0]['variable']
+                for _, row in grp.iterrows():
+                    index = columns.index(row['period'])
+                    table_row[index+1] = row['value']
+                table.append(table_row)
+
+            table = {
+                "class": table_name,
+                "header": [""] + [d.strftime("%d-%m-%Y") for d in columns],
+                "body": table
+            }
+            tables.append(table)
+        self.set_header('Content-Type', 'application/json')
+        self.write({'tables': tables})
 
 
 class Metadata(tornado.web.RequestHandler):
@@ -99,6 +160,7 @@ def make_app():
         (r"/all", AllHandler),
         (r"/meta", Metadata),
         (r"/file", FileHandler),
+        (r"/companydata", CompanyData),
     ])
 
 
