3dadcfa76173e40d5203c09384bd09eae219dba5
manohar <manohar9600@outlook.com>
2023-10-06 18:24:02 +0530

    added fiscal year insertion

--- scripts/company_info.py
+++ scripts/company_info.py
@@ -1,10 +1,12 @@
-# file to fetch data from wikipedia s&p data.
+# file to fetch s&p 500 companies table from wikipedia
 import os
 import requests
 import json
 from tqdm import tqdm
 from bs4 import BeautifulSoup
 import psycopg2
+from glob import glob
+from datetime import datetime
 
 
 def divide_column_rows(table_rows):
@@ -78,12 +80,40 @@ def extract_html_tables(html_content):
     return tables    
 
 
-if __name__ == "__main__":
-    # folder_path = r"C:\Users\Manohar\Desktop\Projects\Finance-Extraction\data\xbrl-documents"
-    # html_path = os.path.join(folder_path, "axp-20221231.htm")
-    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-    r = requests.get(url)
-    html_content = r.text
+def update_fiscal_yrs():
+    for comp_path in glob(r"data\current\*"):
+        result = ''
+        for xbrl_path in glob(os.path.join(comp_path, "*/xbrl_data.json")):
+            with open(xbrl_path, 'r') as f:
+                xbrl_data = json.load(f)
 
-    data = extract_html_tables(html_content)
-    insert_values(data)
+            for dp in xbrl_data.get('factList', []):
+                if dp[2]["label"] == "dei:DocumentPeriodEndDate":
+                    result = datetime.strptime(dp[2]["value"], "%Y-%m-%d").strftime("%m-%d")
+                    break
+            if result:
+                break
+        if result:
+            conn = psycopg2.connect(database="ProdDB",
+                                        host="localhost",
+                                        user="postgres",
+                                        password="manu1234",
+                                        port="5432")
+            cursor = conn.cursor()
+            cursor.execute(f'UPDATE companies SET "Fiscal Year"=\'{result}\' WHERE "Symbol" = \'{os.path.basename(comp_path)}\'')
+            conn.commit()
+            conn.close()
+        
+
+if __name__ == "__main__":
+    # # folder_path = r"C:\Users\Manohar\Desktop\Projects\Finance-Extraction\data\xbrl-documents"
+    # # html_path = os.path.join(folder_path, "axp-20221231.htm")
+    # url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    # r = requests.get(url)
+    # html_content = r.text
+
+    # data = extract_html_tables(html_content)
+    # insert_values(data)
+
+    # code to update company fiscal years by making use of xbrl data
+    update_fiscal_yrs()
