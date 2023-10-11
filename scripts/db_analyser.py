c2be6d15914029c2522fa6fd3916e61bd5a8a6e4
manohar <manohar9600@outlook.com>
2023-10-11 15:12:12 +0530

    detecting empty values

--- scripts/db_analyser.py
+++ scripts/db_analyser.py
@@ -15,6 +15,18 @@ def log_msg(text):
         f.write(text+'\n')
 
 
+def get_variable_name(variableid):
+    conn = psycopg2.connect(database="ProdDB",
+                            host="localhost",
+                            user="postgres",
+                            password="manu1234",
+                            port="5432")
+    cursor = conn.cursor()
+    cursor.execute(f"select variable from variables where id={variableid}")
+    result = cursor.fetchone()[0]
+    return result
+
+
 def get_table_data(table_name):
     conn = psycopg2.connect(database="ProdDB",
                             host="localhost",
@@ -67,14 +79,15 @@ for _, company_row in tqdm(companies_df.iterrows(), total=companies_df.shape[0])
     if not check_value_existence('values', 'companyid', company_row['id']):
         # log_msg(f'{company_row["Symbol"]} not present in database')
         continue
-    
+        
+    # code for checking if there any file's data is not inserted into database or not.
     values_df = get_values_data(company_row['id'])
     periods = values_df['period'].unique()
     files_count = len(glob(f"data/current/{company_row['Symbol']}/*"))
     if len(periods) != files_count:
         log_msg(f'symbol:{company_row["Symbol"]} | count in-difference | files:{files_count}, DB: {len(periods)}')
 
-    # # code for checking missing or extra periods
+    # code for checking missing or extra periods
     # if (datetime.now() - datetime.strptime("2023-" + company_row["Fiscal Year"], 
     #                                        "%Y-%m-%d")).days >= 30:
     #     dates_to_exist = [datetime.strptime(str(yr) + "-" + company_row["Fiscal Year"], 
@@ -98,8 +111,15 @@ for _, company_row in tqdm(companies_df.iterrows(), total=companies_df.shape[0])
     #     else:
     #         log_msg(f'symbol:{company_row["Symbol"]} | period:{datetime.strftime(pe, "%Y-%m-%d")} | is extra')
 
-
-
-
-
-
+    # code to find variables which values are empty
+    empty_values = ''
+    periods_set = set(values_df['period'])
+    for varid, grp in values_df.groupby('variableid'):
+        missing_values = periods_set - set(grp['period'])
+        if missing_values:
+            log_msg(f"variable:{get_variable_name(varid)} | symbol:{company_row['Symbol']} | periods:{[datetime.strftime(p, '%Y-%m-%d') for p in list(missing_values)]} | values are empty")
+    
+    variable_ids = values_df['variableid'].unique()
+    for _, row in variables_df.iterrows():
+        if row['id'] not in variable_ids:
+            log_msg(f"variable:{row['variable']} | symbol:{company_row['Symbol']} | no values for this variable")
